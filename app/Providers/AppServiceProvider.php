dc511f081dd9ca45c3d739bbf553431393c77d60
guest123 <akradechlao1975@gmail.com>
2026-08-08 10:51:57 +0700

    fix: use ServiceProvider instead of RouteServiceProvider (not in Laravel 11)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Cache\RateLimiting\Limit;
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
