6797916716107617b748e5f8d10fc78d1392b664
guest123 <akradechlao1975@gmail.com>
2026-08-08 10:15:19 +0700

    feat: update work_shifts seeder to 16 groups (0-15)

--- database/seeders/WorkShiftSeeder.php
+++ database/seeders/WorkShiftSeeder.php
@@ -10,16 +10,22 @@ class WorkShiftSeeder extends Seeder
     public function run(): void
     {
         $shifts = [
-            ['group_number' => 0, 'start_time' => '07:30', 'end_time' => '16:30', 'work_hours' => 8, 'is_overnight' => false],
-            ['group_number' => 1, 'start_time' => '08:00', 'end_time' => '17:00', 'work_hours' => 8, 'is_overnight' => false],
-            ['group_number' => 2, 'start_time' => '16:00', 'end_time' => '01:00', 'work_hours' => 8, 'is_overnight' => true],
-            ['group_number' => 3, 'start_time' => '00:00', 'end_time' => '09:00', 'work_hours' => 8, 'is_overnight' => true],
-            ['group_number' => 4, 'start_time' => '09:00', 'end_time' => '18:00', 'work_hours' => 8, 'is_overnight' => false],
-            ['group_number' => 5, 'start_time' => '20:00', 'end_time' => '05:00', 'work_hours' => 8, 'is_overnight' => true],
-            ['group_number' => 6, 'start_time' => '21:00', 'end_time' => '06:00', 'work_hours' => 8, 'is_overnight' => true],
-            ['group_number' => 7, 'start_time' => '08:00', 'end_time' => '16:00', 'work_hours' => 7, 'is_overnight' => false],
-            ['group_number' => 8, 'start_time' => '16:00', 'end_time' => '00:00', 'work_hours' => 7, 'is_overnight' => true],
-            ['group_number' => 9, 'start_time' => '00:00', 'end_time' => '08:00', 'work_hours' => 7, 'is_overnight' => true],
+            ['group_number' => 0,  'start_time' => '07:30', 'end_time' => '16:30', 'work_hours' => 8, 'is_overnight' => false],
+            ['group_number' => 1,  'start_time' => '08:00', 'end_time' => '17:00', 'work_hours' => 8, 'is_overnight' => false],
+            ['group_number' => 2,  'start_time' => '16:00', 'end_time' => '01:00', 'work_hours' => 8, 'is_overnight' => true],
+            ['group_number' => 3,  'start_time' => '00:00', 'end_time' => '09:00', 'work_hours' => 8, 'is_overnight' => true],
+            ['group_number' => 4,  'start_time' => '09:00', 'end_time' => '18:00', 'work_hours' => 8, 'is_overnight' => false],
+            ['group_number' => 5,  'start_time' => '20:00', 'end_time' => '05:00', 'work_hours' => 8, 'is_overnight' => true],
+            ['group_number' => 6,  'start_time' => '21:00', 'end_time' => '06:00', 'work_hours' => 8, 'is_overnight' => true],
+            ['group_number' => 7,  'start_time' => '08:00', 'end_time' => '16:30', 'work_hours' => 8, 'is_overnight' => false],
+            ['group_number' => 8,  'start_time' => '16:00', 'end_time' => '00:30', 'work_hours' => 8, 'is_overnight' => true],
+            ['group_number' => 9,  'start_time' => '00:00', 'end_time' => '08:30', 'work_hours' => 8, 'is_overnight' => true],
+            ['group_number' => 10, 'start_time' => '08:00', 'end_time' => '20:00', 'work_hours' => 12, 'is_overnight' => false],
+            ['group_number' => 11, 'start_time' => '20:00', 'end_time' => '08:00', 'work_hours' => 12, 'is_overnight' => true],
+            ['group_number' => 12, 'start_time' => '16:00', 'end_time' => '00:00', 'work_hours' => 8, 'is_overnight' => true],
+            ['group_number' => 13, 'start_time' => '00:00', 'end_time' => '08:00', 'work_hours' => 8, 'is_overnight' => true],
+            ['group_number' => 14, 'start_time' => '07:00', 'end_time' => '16:00', 'work_hours' => 8, 'is_overnight' => false],
+            ['group_number' => 15, 'start_time' => '19:00', 'end_time' => '04:00', 'work_hours' => 8, 'is_overnight' => true],
         ];
 
         foreach ($shifts as $shift) {
