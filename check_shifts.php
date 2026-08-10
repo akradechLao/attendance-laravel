95e10ea98eb72fc88ed41865dbaf69ffaf28efe5
guest123 <akradechlao1975@gmail.com>
2026-08-10 14:47:01 +0700

    fix: use workShifts relationship

--- check_shifts.php
+++ check_shifts.php
@@ -4,9 +4,9 @@ $app = require_once __DIR__.'/bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->handle(new \Symfony\Component\Console\Input\StringInput(''), new \Symfony\Component\Console\Output\NullOutput());
 
-$es = \App\Models\Employee::where('company_id',1)->with('shifts')->get();
+$es = \App\Models\Employee::where('company_id',1)->with('workShifts')->get();
 foreach($es as $e){
-    $times = $e->shifts->map(function($s){
+    $times = $e->workShifts->map(function($s){
         return $s->group_type.'('.$s->start_time.'-'.$s->end_time.')';
     })->implode(', ');
     echo $e->employee_code.' '.$e->name.' | '.($times ?: 'no shift').PHP_EOL;
